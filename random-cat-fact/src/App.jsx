0785e3f2accde1b01075e74c014c686a004dc1d2
ejmorian <ejmorian@gmail.com>
2023-03-27 12:50:44 +0800

    Refactor code for easy readability

--- random-cat-fact/src/App.jsx
+++ random-cat-fact/src/App.jsx
@@ -5,22 +5,21 @@ import './App.css'
 function App() {
 
   const [fact, setFact] = useState('');
-  const [next, setNext] = useState(false);
 
-  useEffect(() => {
+  const fetchCatFact = () => {
     fetch("https://catfact.ninja/fact").then(response => response.json()).then(data => setFact(data.fact))
-  }, [next])
+  }
+
+  useEffect(() => fetchCatFact(), [])
 
   return (
     <div>
       <h2>{fact}</h2>
       <h3>Random Cat Facts</h3>
-      <button onClick={() => !next ? setNext(true) : setNext(false)}>Click Me</button>
-      {next}
+      <button onClick={fetchCatFact}>Click Me</button>
     </div>
-
-
   )
 }
+
 
 export default App
