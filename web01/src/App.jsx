6c1008daae41e8e97a2aa6f786829e463338ecc4
ejmorian <ejmorian@gmail.com>
2023-03-28 23:53:31 +0800

    Style Nav

--- web01/src/App.jsx
+++ web01/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import Navigation from './components/Navigation'
 
 import './App.css'
 
@@ -10,7 +11,8 @@ function App() {
 
   return (
     <div>
-      <h1>Hello</h1>
+      <Navigation />
+
 
     </div>
 
